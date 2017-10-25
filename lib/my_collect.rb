67cb7e622cf13b6c347ed52eceb8d8ab2395e14d
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-25 18:19:28 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,7 +9,7 @@ def my_collect(array)
 end
 
 
-#collection = ['ruby', 'javascript', 'python', 'objective-c']
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
   lang.upcase
 end
