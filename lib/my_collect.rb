5586aad9208e6a9f29c1744a2d62d00644af8b13
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-25 18:18:49 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,20 @@
-def my_collect(collection)
+def my_collect(array)
   i = 0
 
-  while i < collection.length
-    yield(collection[i])
+  while i < array.length
+    yield(array[i])
     i+=1
   end
-  collection
+  array
 end
 
 
-collection = ['ruby', 'javascript', 'python', 'objective-c']
+#collection = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(collection) do |lang|
   lang.upcase
+end
+
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
 end
