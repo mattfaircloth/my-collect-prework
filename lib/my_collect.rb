fc361aa026f3fdd8e17c5dba3877ab50bd41c29a
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-25 18:20:55 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -14,7 +14,7 @@ my_collect(collection) do |lang|
   lang.upcase
 end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(students) do |name|
   name.split(" ").first
 end
