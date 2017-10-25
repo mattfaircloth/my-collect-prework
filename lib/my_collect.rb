3a11d2bf02694b4b01730a8c568263ab83a0ecd5
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-25 18:46:46 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   i = 0
  final_array = []
   while i < array.length
-    yield(final_array << array[i])
+    yield(array[i])
     i = i + 1
   end
-  final_array
+  array
 end
